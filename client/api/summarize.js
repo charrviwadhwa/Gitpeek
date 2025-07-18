5ce6e90cc06ca29260c3dc9e6b53e9fd0f456b8d
charviwadhwa02 <charviwadhwa02@gmail.com>
2025-07-19 00:24:42 +0530

    changes

--- client/api/summarize.js
+++ client/api/summarize.js
@@ -2,6 +2,7 @@
 import axios from "axios";
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
+
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const MAX_FILES = 10;
